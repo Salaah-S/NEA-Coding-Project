c9b7f480432735f4a0288262dc945a87276a7792
Salaah-S <149571021+Salaah-S@users.noreply.github.com>
2024-11-05 21:43:44 +0000

    lol

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 import pygame
+import sys
+from startup import bg_sprite
 
+
+# Initialised the pygame library
+pygame.init()
+
+# Defining the screen width and height
+screen_width, screen_height = 240, 160
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+
+    screen.blit(bg_sprite)
+
+    pygame.display.flip()
+    
+
+
+
+pygame.quit()
+sys.exit()
