f1c0f72577a5ec6f8bee9d29e05e230df3ee8ab5
Salaah-S <149571021+Salaah-S@users.noreply.github.com>
2024-11-17 07:41:32 +0000

    Made the Character Class a sprite class, and does everything that the old one did

--- player.py
+++ player.py
@@ -1,121 +1,103 @@
-from pygame.sprite import _Group
 import spritesheet
 import pygame
 
-class Character():
-    def __init__(self, name, gender, sprite_loc, screen_width, screen_height):
-        self.name = name
-        self.gender = gender
-        self.sprite_loc = sprite_loc
-        self.current_frame = 1
-        self.screen_width = screen_width
-        self.screen_height = screen_height
+class Character(pygame.sprite.Sprite):
 
+    def __init__(self, x, y, loc, screen_width, screen_height):
+        super().__init__()
+        self.screen_height = screen_height
+        self.screen_width = screen_width
+        self.previous = 0 
+        self.loc = spritesheet.Spritesheet(loc)
+        self.sprites = []
+        for i in range(12):
+            self.sprites.append(self.loc.get_sprite((i), 16,32,3))
+            if i == 2 or i == 5 or i == 8 or i == 11:
+                self.sprites.append(self.loc.get_sprite(i-1, 16,32,3))
+
+
+        self.current_frame = 1
+
+        self.image = self.sprites[self.current_frame]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.previous_position = self.rect.topleft
+        
+    def update(self, direction, obstacles):
+        self.previous_position = self.rect.topleft
+
+        if direction[pygame.K_UP]:
+            self.previous = pygame.K_UP
+            animation_list = []
+            for i in range(4):
+                animation_list.append(self.sprites[i+4])
+            now = animation_list[self.current_frame]
+
+            self.rect.top -= 8
+            if self.rect.top < 0:
+                self.rect.top = 0
+
+            self.current_frame +=1
+            if self.current_frame == 4:
+                self.current_frame = 0
+        
+        elif direction[pygame.K_DOWN]:
+            self.previous = pygame.K_DOWN
+            animation_list = []
+            for i in range(4):
+                animation_list.append(self.sprites[i])
+            now = animation_list[self.current_frame]
+
+            self.rect.bottom += 8
+            if self.rect.bottom > self.screen_height:
+                self.rect.bottom = self.screen_height
+            self.current_frame +=1
+            if self.current_frame == 4:
+                self.current_frame = 0
+
+        elif direction[pygame.K_LEFT]:
+            self.previous = pygame.K_LEFT
+            animation_list = []
+            for i in range(4):
+                animation_list.append(self.sprites[i+8])
+            now = animation_list[self.current_frame]
+
+            self.rect.left -= 8
+            if self.rect.left <0:
+                self.rect.left = 0
+            self.current_frame +=1
+            if self.current_frame == 4:
+                self.current_frame = 0
+
+        elif direction[pygame.K_RIGHT]:
+            self.previous = pygame.K_RIGHT
+            animation_list = []
+            for i in range(4):
+                animation_list.append(self.sprites[i+12])
+            now = animation_list[self.current_frame]
+
+            self.rect.right += 8
+            if self.rect.right > self.screen_width:
+                self.rect.right = self.screen_width
+            self.current_frame +=1
+            if self.current_frame == 4:
+                self.current_frame = 0
+        else:
+            now = self.sprites[self.idle(self.previous)]
+        
+        if pygame.sprite.spritecollide(self, obstacles, False):
+            self.rect.topleft = self.previous_position
+        
+        self.image = now
+        return now
+    
     def idle(self, key):
-        sprite = spritesheet.Spritesheet(self.sprite_loc)
 
         keys = {
             pygame.K_DOWN : 1,
-            pygame.K_UP : 4,
-            pygame.K_LEFT : 7,
-            pygame.K_RIGHT: 10,
+            pygame.K_UP : 5,
+            pygame.K_LEFT : 9,
+            pygame.K_RIGHT: 13,
             0:1
         }
-
-        return sprite.get_sprite(keys.get(key), 16,32,3)
-
-
-    def walking(self, direction, x, y, previous):
-        sprite = spritesheet.Spritesheet(self.sprite_loc)
-        
-        if direction[pygame.K_DOWN]:
-            
-            animation_list = []
-            for i in range(3):
-                animation_list.append(sprite.get_sprite(i, 16,32,3))
-            idle_frame = sprite.get_sprite(1, 16,32,3)
-            animation_list.append(idle_frame)
-
-            now = animation_list[self.current_frame]
-            y += 8
-            print(y)
-            if y >= self.screen_height - 32:
-                y = self.screen_height - 32
-            self.current_frame +=1
-            if self.current_frame == 4:
-                self.current_frame = 0
-
-            previous = pygame.K_DOWN
-
-        elif direction[pygame.K_UP]:
-        
-            animation_list = []
-            for i in range(3):
-                animation_list.append(sprite.get_sprite((i+3), 16,32,3))
-            idle_frame = sprite.get_sprite(4, 16,32,3)
-            animation_list.append(idle_frame)
-
-            now = animation_list[self.current_frame]
-            y -= 8
-            if y < 0:
-                y = 0
-            self.current_frame +=1
-            if self.current_frame == 4:
-                self.current_frame = 0
-
-            previous = pygame.K_UP
-
-        elif direction[pygame.K_LEFT]:
-        
-            animation_list = []
-            for i in range(3):
-                animation_list.append(sprite.get_sprite((i+6), 16,32,3))
-            idle_frame = sprite.get_sprite(7, 16,32,3)
-            animation_list.append(idle_frame)
-
-            now = animation_list[self.current_frame]
-            x -= 8
-            if x < 0:
-                x = 0
-            self.current_frame +=1
-            if self.current_frame == 4:
-                self.current_frame = 0
-
-            previous = pygame.K_LEFT
-
-        elif direction[pygame.K_RIGHT]:
-        
-            animation_list = []
-            for i in range(3):
-                animation_list.append(sprite.get_sprite((i+9), 16,32,3))
-            idle_frame = sprite.get_sprite(10, 16,32,3)
-            animation_list.append(idle_frame)            
-
-            now = animation_list[self.current_frame]
-            x += 8
-            if x >= self.screen_width - 16:
-                x = self.screen_width - 16
-            self.current_frame +=1
-            if self.current_frame == 4:
-                self.current_frame = 0
-
-            previous = pygame.K_RIGHT
-
-        else:
-            now = self.idle(previous)
-
-        return now, x, y, previous
-    
-class Sprite(pygame.sprite.Sprite, Character):
-
-    def __init__(self, sprite_width, sprite_height, screen_width, screen_height, loc, current_frame):
-        super().__init__()
-        self.current = current_frame
-        self.image = spritesheet.Spritesheet(loc).get_sprite(self.current, sprite_width, sprite_height, 3)
-        self.rect = self.image.get_rect()
-
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-
-    def update(direction, x, y, previous):
-        
+        return keys[key]
