635828a217ac865780b389a126bedaf5815dd786
Salaah-S <149571021+Salaah-S@users.noreply.github.com>
2025-01-18 07:59:42 +0000

    V2.1 Midway

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ class Main_Game():
     def __init__(self, screen_width, screen_height, tile_size):
         # Here, declaring the screen 
         self.screen = pygame.display.set_mode((screen_width, screen_height))
+        self.game_loop_state = False
         self.start_menu_state = True
+        self.title_screen_state = True
+        self.settings_state = False
         self.user = player.Character(x,y,'assets/character/red_walking.png', screen_width, screen_height, tile_size)
         oak = player.Character(120,160, 'assets/character/oak.png', screen_width, screen_height, tile_size)
         self.obstacle = pygame.sprite.Group()
         self.obstacle.add(oak)
 
+
     def start_menu(self,key_press):
         self.screen.fill('black')
-        font = pygame.font.Font(None, 60)
 
         logo = spritesheet.Spritesheet('assets/logo.png').get_sprite(0,172, 104,3, (255,255,255))
         logo_rect = logo.get_rect(center=(360, 200))
@@ -37,14 +40,64 @@ class Main_Game():
             self.screen.blit(text,((count+228),400) )
             count+=(6*4)
         
-
-        
         self.screen.blit(logo, logo_rect)
+
         if key_press[pygame.K_LSHIFT] or key_press[pygame.K_RSHIFT]:
             self.start_menu_state = False
-        else:
-            self.start_menu_state == True
     
+    def title_screen(self, key_press):
+        
+        self.screen.fill('black')
+        save_text = self.font_writing('New Game    Press A', 'assets/dark_font.png')
+        settings_text = self.font_writing('Settings    Press B', 'assets/dark_font.png')
+
+        count = 0
+        for text in save_text:
+            self.screen.blit(text,((count+150),150) )
+            count+=(6*4)
+        count = 0
+        for text in settings_text:
+            self.screen.blit(text,((count+150),300) )
+            count+=(6*4)
+
+        if key_press[pygame.K_a]:
+            self.title_screen_state = False
+        elif key_press[pygame.K_b]:
+            self.settings_state = True
+            self.title_screen_state = False
+
+
+    def settings(self, key_press):
+        self.screen.fill('black')
+
+    
+        text_speed_text = self.font_writing('Select Text Speed','assets/dark_font.png' )
+        count = 0
+        for text in text_speed_text:
+            self.screen.blit(text,((count+150),150) )
+            count+=(6*4)
+
+        speed = {
+            0: 'Slow',
+            1: 'Fast'
+        }
+        current_speed = speed[0]
+        speed_options = self.font_writing(f"{current_speed}    Press A to change", 'assets/dark_font.png')
+        count = 0
+        for text in speed_options:
+            self.screen.blit(text,((count+150),300) )
+            count+=(6*4)
+
+        if key_press[pygame.K_a]:
+            if current_speed == speed[0]:
+                current_speed = speed[1]
+            else:
+                current_speed = speed[0]
+        
+
+
+
+
     def font_writing(self, text, font):
         letter_dict = {
             'A':0,
@@ -100,7 +153,12 @@ class Main_Game():
 
             if self.start_menu_state:
                 self.start_menu(keys)
-            else:
+            elif self.title_screen_state:
+                self.title_screen(keys)
+            elif self.settings_state:
+                self.settings(keys)
+
+            elif self.game_loop_state:
                 self.user.update(keys, self.obstacle)
                 self.screen.blit(self.user.image, self.user.rect)
                 self.obstacle.draw(self.screen)
